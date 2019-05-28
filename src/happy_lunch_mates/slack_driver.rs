21ac936997c9519688859263788f88cf338f286e
Hatef Khadivinassab <hatef.khadivinassab@gmail.com>
2019-05-28 09:31:23 -0700

    added slack communications

--- src/happy_lunch_mates/slack_driver.rs
+++ src/happy_lunch_mates/slack_driver.rs
@@ -1,20 +1,93 @@
 use std::vec::Vec;
+extern crate slack_api as slack;
+
+use dotenv::dotenv;
+use slack::requests::{Error, Client};
 
 #[derive(Debug)]
-pub struct SlackDriver<'sch> {
-    channel_name : &'sch str,
-    pub channel_members: Vec<(&'sch str, &'sch str)>,
+pub struct SlackDriver {
+    slack_client: slack::requests::Client,
+    slack_token : std::string::String,
+
+    
 }
 
-impl<'sch> SlackDriver<'sch> {
-    pub fn new (channel_name: &str) -> SlackDriver {
+impl SlackDriver {
+    pub fn new() -> SlackDriver {
+        let token = std::env::var("AUTH_TOKEN").expect("AUTH_TOKEN not set in .env");
+        let client: slack::requests::Client = slack::default_client().unwrap();
+
         SlackDriver {
-            channel_name: channel_name,
-            channel_members: Vec::new()
+            slack_client: client,
+            slack_token : token,
         }
     }
 
-    pub fn get_members(&mut self, channel_members: &Vec<(&'sch str, &'sch str)>) {
-        self.channel_members = channel_members.to_vec();
+
+    // pub fn get_members(&mut self, channel_members: &Vec<&'sd str>) {
+    //     self.channel_members = channel_members.to_vec();
+    // }
+    pub fn get_members(&self, channel_name: &str) {
+
+        let response = slack::users::list(&self.slack_client,
+                                        &self.slack_token,
+                                        &slack::users::ListRequest::default());
+
+        let response = slack::channels::list(&self.slack_client,
+                                        &self.slack_token,
+                                        &slack::channels::ListRequest::default());
+
+        if let Ok(response) = response {
+            // println!("{:?}", response.channels);
+            if let Some(channels) = response.channels {
+                println!("Got {} channels", channels.len());
+
+                for channel in channels {
+                    if &channel.name.unwrap() == channel_name {
+                        println!("{:?}", channel.members);
+                    }
+                }
+            }
+            // if let Some(users) = response.members {
+            //     println!("Got {} users", users.len());
+            //     for user in users {
+            //         println!("{:?}", user.real_name);
+            //     }
+            // }
+        } else {
+            println!("{:?}", response);
+        }
+    }
+
+    pub fn _get_channels_list(&self) -> Vec<slack::Channel> {
+        let mut channels_list: Vec<slack::Channel> = Vec::new();
+        let response = slack::channels::list(&self.slack_client,
+                                &self.slack_token,
+                                &slack::channels::ListRequest::default());
+
+        if let Ok(response) = response {
+            if let Some(channels) = response.channels {
+                channels_list = channels;    
+            }
+        } else {
+        println!("{:?}", response);
+        }
+        return channels_list;
+    }
+
+    pub fn _get_users_list(&self) -> Vec<slack::User> {
+        let mut users_list: Vec<slack::User> = Vec::new();
+        let response = slack::users::list(&self.slack_client,
+                                &self.slack_token,
+                                &slack::users::ListRequest::default());
+
+        if let Ok(response) = response {
+            if let Some(users) = response.members {
+                users_list = users;    
+            }
+        } else {
+        println!("{:?}", response);
+        }
+        return users_list;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 mod happy_lunch_mates;
+use dotenv::dotenv;
 
 fn main() {
-    let channel_memebers = vec![("Hatef", "Khadivi"),
-                                 ("Connor", "Shannon"), 
-                                 ("Nigel", "Myers"), 
-                                 ("Jono", "Chapple"), 
-                                 ("Jon", "Moore"), 
-                                 ("Kevin", "Wu"),
-                                 ("Holly", "Peck"),
-                                 ("Olivia", "Norton"),
-                                 ("Geordie", "Rose"),
-                                 ("Suzanne", "Gildert")];
+    dotenv().ok();
+    let channel_memebers = vec!["Hatef",
+                                 "Connor", 
+                                 "Nigel", 
+                                 "Jono", 
+                                 "Jon", 
+                                 "Kevin",
+                                 "Holly",
+                                 "Olivia",
+                                 "Geordie",
+                                 "Suzanne"];
 
     let restaurant_names = vec!["Pho", "Argos", "Soosh", "RailTown", "Bao", "BrewHall"];
     let n_groups = 3;
 
     let channel_name: &str = "lunch-crew";
 
-    let mut slack: happy_lunch_mates::slack_driver::SlackDriver = happy_lunch_mates::slack_driver::SlackDriver::new(&channel_name);
+    // let AUTH_TOKEN: std::result::Result<std::string::String, std::env::VarError> = std::env::var("AUTH_TOKEN");
 
-    slack.get_members(&channel_memebers);
+    // println!("{:?}", AUTH_TOKEN);
 
-    println!("channel members: {:?}", slack.channel_members);
 
-    let mut hlm_test: happy_lunch_mates::HappyLunchMates = happy_lunch_mates::HappyLunchMates::new(&slack, &n_groups, &restaurant_names);
+    let mut slack: happy_lunch_mates::slack_driver::SlackDriver = happy_lunch_mates::slack_driver::SlackDriver::new();
 
-    hlm_test.assign_to_group();
+    println!("{:?}", slack._get_users_list());
+
+    // println!("channel members: {:?}", slack.channel_members);
+
+    // let mut hlm_test: happy_lunch_mates::HappyLunchMates = happy_lunch_mates::HappyLunchMates::new(&slack, &n_groups, &restaurant_names);
+
+    // hlm_test.assign_to_group();
+
+
 }
 
 
